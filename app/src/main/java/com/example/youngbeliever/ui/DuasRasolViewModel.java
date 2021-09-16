4c41b9030d29e07677c16a469a554b79261be626
Seif El-Deen Tarek <seif.eldeen.tarek.pro@gmail.com>
2021-09-16 11:16:49 +0200

    Added Duas Strings

--- app/src/main/java/com/example/youngbeliever/ui/DuasRasolViewModel.java
+++ app/src/main/java/com/example/youngbeliever/ui/DuasRasolViewModel.java
@@ -20,22 +20,35 @@ public class DuasRasolViewModel extends ViewModel
     private ArrayList<DuasModel> getDuaFromDataBase()
     {
         ArrayList<DuasModel> duasList = new ArrayList<>();
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
-        duasList.add(new DuasModel(R.string.asm_meaning72,R.string.asm_meaning7));
+        duasList.add(new DuasModel(R.string.rasol_dua1,R.string.rasol_dua_info1));
+        duasList.add(new DuasModel(R.string.rasol_dua2,R.string.rasol_dua_info2));
+        duasList.add(new DuasModel(R.string.rasol_dua3,R.string.rasol_dua_info3));
+        duasList.add(new DuasModel(R.string.rasol_dua4,R.string.rasol_dua_info4));
+        duasList.add(new DuasModel(R.string.rasol_dua5,R.string.rasol_dua_info5));
+        duasList.add(new DuasModel(R.string.rasol_dua6,R.string.rasol_dua_info6));
+        duasList.add(new DuasModel(R.string.rasol_dua7,R.string.rasol_dua_info7));
+        duasList.add(new DuasModel(R.string.rasol_dua8,R.string.rasol_dua_info8));
+        duasList.add(new DuasModel(R.string.rasol_dua9,R.string.rasol_dua_info9));
+        duasList.add(new DuasModel(R.string.rasol_dua10,R.string.rasol_dua_info10));
+        duasList.add(new DuasModel(R.string.rasol_dua11,R.string.rasol_dua_info11));
+        duasList.add(new DuasModel(R.string.rasol_dua12,R.string.rasol_dua_info12));
+        duasList.add(new DuasModel(R.string.rasol_dua13,R.string.rasol_dua_info13));
+        duasList.add(new DuasModel(R.string.rasol_dua14,R.string.rasol_dua_info14));
+        duasList.add(new DuasModel(R.string.rasol_dua15,R.string.rasol_dua_info15));
+        duasList.add(new DuasModel(R.string.rasol_dua16,R.string.rasol_dua_info16));
+        duasList.add(new DuasModel(R.string.rasol_dua17,R.string.rasol_dua_info17));
+        duasList.add(new DuasModel(R.string.rasol_dua18,R.string.rasol_dua_info18));
+        duasList.add(new DuasModel(R.string.rasol_dua19,R.string.rasol_dua_info19));
+        duasList.add(new DuasModel(R.string.rasol_dua20,R.string.rasol_dua_info20));
+        duasList.add(new DuasModel(R.string.rasol_dua21,R.string.rasol_dua_info21));
+        duasList.add(new DuasModel(R.string.rasol_dua22,R.string.rasol_dua_info22));
+        duasList.add(new DuasModel(R.string.rasol_dua23,R.string.rasol_dua_info23));
+        duasList.add(new DuasModel(R.string.rasol_dua24,R.string.rasol_dua_info24));
+        duasList.add(new DuasModel(R.string.rasol_dua25,R.string.rasol_dua_info25));
+        duasList.add(new DuasModel(R.string.rasol_dua26,R.string.rasol_dua_info26));
+        duasList.add(new DuasModel(R.string.rasol_dua27,R.string.rasol_dua_info27));
+        duasList.add(new DuasModel(R.string.rasol_dua18,R.string.rasol_dua_info28));
+        duasList.add(new DuasModel(R.string.rasol_dua29,R.string.rasol_dua_info29));
         return duasList;
     }
 }
--- app/src/main/java/com/example/youngbeliever/ui/DuasRoslViewModel.java
+++ app/src/main/java/com/example/youngbeliever/ui/DuasRoslViewModel.java
@@ -20,26 +20,19 @@ public class DuasRoslViewModel extends ViewModel
     private ArrayList<DuasModel> getDuaFromDataBase()
     {
         ArrayList<DuasModel> duasList = new ArrayList<>();
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
-        duasList.add(new DuasModel(R.drawable.color_white, R.string.asm_meaning24, R.string.asm_meaning19));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua1, R.string.rosl_dua_info1));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua2, R.string.rosl_dua_info2));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua3, R.string.rosl_dua_info3));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua4, R.string.rosl_dua_info4));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua5, R.string.rosl_dua_info5));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua6, R.string.rosl_dua_info6));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua7, R.string.rosl_dua_info7));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua8, R.string.rosl_dua_info8));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua9, R.string.rosl_dua_info9));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua10, R.string.rosl_dua_info10));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua11, R.string.rosl_dua_info11));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua12, R.string.rosl_dua_info12));
+        duasList.add(new DuasModel(R.drawable.color_white, R.string.rosl_dua13, R.string.rosl_dua_info13));
         return duasList;
     }
 }
