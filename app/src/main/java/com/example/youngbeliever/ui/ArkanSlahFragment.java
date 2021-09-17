ea20e87fea90d982a158d1f9ee34d8b930c5f1f1
Seif El-Deen Tarek <seif.eldeen.tarek.pro@gmail.com>
2021-09-17 16:57:00 +0200

    SlahScreen(FIXED video_view)&& ui ADJ

--- app/src/main/java/com/example/youngbeliever/ui/ArkanSlahFragment.java
+++ app/src/main/java/com/example/youngbeliever/ui/ArkanSlahFragment.java
@@ -1,13 +1,22 @@
 package com.example.youngbeliever.ui;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -16,13 +25,25 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.youngbeliever.R;
 import com.example.youngbeliever.pojo.ArkanSlahModel;
+import com.github.barteksc.pdfviewer.util.Util;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 public class ArkanSlahFragment extends Fragment
 {
     ArkanSlahViewModel arkanSlahViewModel;
 
+    MediaController mediaController;
+    VideoView slahVideoView;
+    String videoPath;
+    int videoName;
+    Uri uri;
+    MaterialCardView slahContainer;
+    RelativeLayout layout;
     public ArkanSlahFragment()
     {
     }
@@ -36,6 +57,17 @@ public class ArkanSlahFragment extends Fragment
         arkanSlahViewModel = ViewModelProviders.of(this).get(ArkanSlahViewModel.class);
 
         RecyclerView slahRecycler = rootView.findViewById(R.id.arkan_slah_recycler);
+
+        FloatingActionButton floatingActionButton = rootView.findViewById(R.id.fab);
+
+        layout = rootView.findViewById(R.id.page);
+
+        slahVideoView = rootView.findViewById(R.id.slah_video_view);
+        slahContainer = rootView.findViewById(R.id.slah_video_container);
+
+        mediaController = new MediaController(getActivity());
+        slahVideoView.setMediaController(mediaController);
+        mediaController.setAnchorView(slahVideoView);
 
         ArkanSlahAdapter adapter = new ArkanSlahAdapter();
         slahRecycler.setAdapter(adapter);
@@ -52,9 +84,24 @@ public class ArkanSlahFragment extends Fragment
                     @Override
                     public void onItemClick(ArkanSlahModel arkanSlahModel)
                     {
-                        intent(arkanSlahModel.getSlahWay());
+                        videoName = arkanSlahModel.getSlahWay();
+                        slahContainer.setVisibility(View.VISIBLE);
+                        floatingActionButton.setVisibility(View.VISIBLE);
+
+                        setUserVisibleHint(true);
                     }
                 });
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                slahContainer.setVisibility(View.GONE);
+                slahVideoView.stopPlayback();
+                floatingActionButton.setVisibility(View.GONE);
             }
         });
 
@@ -63,10 +110,77 @@ public class ArkanSlahFragment extends Fragment
         return rootView;
     }
 
-    private void intent(int name)
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser)
     {
-        Intent intent = new Intent(getContext(), SlahVideo.class);
-        intent.putExtra("name" , name);
-        startActivity(intent);
+        super.setUserVisibleHint(isVisibleToUser);
+        if (this.isVisible())
+        {
+            if (!isVisibleToUser)
+            {
+                slahVideoView.stopPlayback();
+            } else {
+                switch (videoName)
+                {
+                    case R.string.slah_way_1:
+                        videoPath = "android.resource://" + requireActivity().getPackageName() + "/" + R.raw.one;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        slahVideoView.start();
+                        break;
+                    case R.string.slah_way_2:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.two;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        slahVideoView.start();
+                        break;
+                    case R.string.slah_way_3:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.three;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        break;
+                    case R.string.slah_way_4:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.four;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        break;
+                    case R.string.slah_way_5:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.five;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        break;
+                    case R.string.slah_way_6:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.six;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        break;
+                    case R.string.slah_way_7:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.seven;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        break;
+                    case R.string.slah_way_8:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.eight;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        break;
+                    case R.string.slah_way_9:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.nine;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        break;
+                    case R.string.slah_way_10:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.ten;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        break;
+                    case R.string.slah_way_11:
+                        videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.eleven;
+                        uri = Uri.parse(videoPath);
+                        slahVideoView.setVideoURI(uri);
+                        break;
+                }
+            }
+        }
     }
 }
